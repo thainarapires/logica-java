1a671018719157e770e82e2f023b41c8b50dca8b
thainarapires <thainara.psilva7@senacsp.edu.br>
2023-06-13 17:15:26 -0300

    Att

---
+++ AcessoBanco-2/src/main/java/com/projeto/bancoacesso/AcessoBanco2Application.java
@@ -0,0 +1,13 @@
+package com.projeto.bancoacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoBanco2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoBanco2Application.class, args);
+	}
+
+}
---
+++ AcessoBanco-2/src/main/java/com/projeto/bancoacesso/controller/ProdutosController.java
@@ -0,0 +1,68 @@
+package com.projeto.bancoacesso.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.bancoacesso.models.Produtos;
+import com.projeto.bancoacesso.repository.ProdutosRepository;
+
+@RestController
+public class ProdutosController {
+	
+	@Autowired
+	private ProdutosRepository produtosRepo;
+	
+	@GetMapping("/produtos/listar")
+	public List<Produtos> listar() {
+		return produtosRepo.findAll();
+	}
+	
+	@PostMapping("/produtos/cadastrar")
+	public String cadastrar(@RequestBody Produtos produtos) {
+		
+		produtosRepo.save(produtos);
+		return "Cadastrou";
+	}
+	
+	@PutMapping("/produtos/atualizar/{id}")
+	public String atualizar(@PathVariable Integer id, @RequestBody Produtos produtos) {
+		 String msg = "";
+		 Optional<Produtos> c = produtosRepo.findById(id);
+		 
+		 if( c.isPresent()) {
+			 produtos.setIdproduto(id);
+			 produtosRepo.save(produtos);
+			 msg = "Produto atualizado";
+			 }
+		 else {
+			 msg = "Produto nao encontrado";
+			 
+		 }
+		 return msg;
+	}
+	@PutMapping("/produtos/atualizar/{id}")
+	public String apagar(@PathVariable Integer id) {
+		 String msg = "";
+		 Optional<Produtos> c = produtosRepo.findById(id);
+		 
+		 if( c.isPresent()) {
+			
+			 produtosRepo.deleteById(id);
+			 msg = "Produto apagado";
+			 }
+		 else {
+			 msg = "Produto nao encontrado";
+			 
+		 }
+		 return msg;
+	}
+
+}
